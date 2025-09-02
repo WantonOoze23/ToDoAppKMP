c6440e9b3a92d18875e0a67dee507c18afbdfe8d
Daniil <tyshko.daniil@lll.kpi.ua>
2025-09-02 15:30:13 +0300

    web test on different platforms

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -127,4 +127,5 @@ dependencies {
     add("kspIosSimulatorArm64", libs.androidx.room.compiler)
     add("kspIosX64", libs.androidx.room.compiler)
     add("kspIosArm64", libs.androidx.room.compiler)
+    add("kspWasmJs", libs.androidx.room.compiler)
 }
